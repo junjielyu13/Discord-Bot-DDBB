0c7bfb7862c423c8e2155c88e7f1036e83fdf28d
Junjie_Li <jfjunjieli@gmail.com>
2023-02-28 17:37:14 +0100

    update: env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { DiscordModule } from '@discord-nestjs/core';
+import { GatewayIntentBits } from 'discord.js';
+
+import * as dotenv from 'dotenv';
+
+const TOKEN = process.env.BOT_TOKEN;
+dotenv.config();
+
 @Module({
-  imports: [],
+  imports: [
+    DiscordModule.forRootAsync({
+      useFactory: () => ({
+        token: process.env.BOT_TOKEN,
+        discordClientOptions: {
+          intents: [GatewayIntentBits.Guilds],
+        },
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
