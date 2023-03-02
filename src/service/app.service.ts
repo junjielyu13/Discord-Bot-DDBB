f35ad9fea4b8a46541488dea29e9eeb6d8a47d76
Junjie_Li <jfjunjieli@gmail.com>
2023-03-02 16:55:27 +0100

    fix: npm run start:Dev

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Wo rld!';
+    return 'Hes dsd ds d s s o Wo rld!';
   }
 }
