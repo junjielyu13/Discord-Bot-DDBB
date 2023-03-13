9105cf52da06d58c08eb979569624c0f9aa80b2d
Junjie_Li <jfjunjieli@gmail.com>
2023-03-14 00:33:35 +0100

    message handle

--- src/gateways/bot.gateway.ts
+++ src/gateways/bot.gateway.ts
@@ -1,6 +1,8 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Once, InjectDiscordClient } from '@discord-nestjs/core';
-import { Client } from 'discord.js';
+import { On, Once, InjectDiscordClient } from '@discord-nestjs/core';
+import { Client, Message } from 'discord.js';
+
+import { EmbedBuilder } from 'discord.js';
 
 @Injectable()
 export class BotGateway {
@@ -13,5 +15,27 @@ export class BotGateway {
   @Once('ready')
   onReady() {
     this.logger.log(`Bot ${this.client.user.tag} was started!`);
+  }
+
+  @On('messageCreate')
+  async onMessage(message: Message): Promise<void> {
+    // const mentionedUser = message.mentions.users.first();
+    if (!message.author.bot) {
+      const userId = message.author.id;
+      const username = message.author.username;
+      const channelId = message.channelId;
+      const messageId = message.id;
+      const messageContent = message.content;
+
+      await message.reply(`
+      User id: ${userId}
+      User name: ${username} 
+      Channel Id: ${channelId} 
+      Message Id: ${messageId} 
+      Message Content: ${messageContent} 
+      `);
+    }
+
+    // TODO SAVE ON DDBB
   }
 }
