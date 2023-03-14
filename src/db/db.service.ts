96f8fd9ae5b31cb82c9e0ae23afb2691a8771a27
Junjie_Li <jfjunjieli@gmail.com>
2023-03-14 01:24:54 +0100

    feat: ddbb

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { User as UserModel, Post as PostModel, Prisma } from '@prisma/client';
+// import { User as UserModel, Post as PostModel, Prisma } from '@prisma/client';
 
 export class DBService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async addUser(): Promise<any> {
-    return this.prismaService.user.create({
-      data: {
-        name: '9999999999999',
-        email: '99999999999999999999',
-      },
-    });
+    // return this.prismaService.user.create({
+    //   data: {
+    //     name: '9999999999999',
+    //     email: '99999999999999999999',
+    //   },
+    // });
   }
 }
--- src/prisma/prisma.controller.ts
+++ src/prisma/prisma.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Post, Req, Get, Body, Query, Param } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { User as UserModel, Post as PostModel, Prisma } from '@prisma/client';
+// import { User as UserModel, Post as PostModel, Prisma } from '@prisma/client';
 
 @Controller('/prisma')
 export class PrismaController {
@@ -10,8 +10,10 @@ export class PrismaController {
   async createDraft(): Promise<any> {
     return this.prismaService.user.create({
       data: {
-        name: '10000000000000000000',
-        email: '1000000000000000000000',
+        user_id: 1632,
+        user_name: '100000 00000000000000',
+        role: '1000000000000000000000',
+        status: '10000000000000000000',
       },
     });
   }
